b9fc38ce66f88d3367d4d05b035af983850582d1
theprince123t-create <theprince123t@gmail.com>
2025-08-15 17:00:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// public folder serve karega
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+  console.log(`✅ CricHeroes overlay server running on port ${PORT}`);
 });
